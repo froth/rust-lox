b16356d9de1097226d651c42871f4a6e518b695e
Fred Roth <f-roth@megaera.de>
2024-01-21 14:11:37 +0100

    start using miette

--- src/lox.rs
+++ src/lox.rs
@@ -20,18 +20,18 @@ impl Lox {
         }
     }
 
-    fn run(&mut self, source: String) {
-        let mut scanner = Scanner::new(source, self.error_reporter.as_mut());
+    fn run(&mut self, source: String, filename: String) {
+        let mut scanner = Scanner::new(source,filename, self.error_reporter.as_mut());
         let tokens = scanner.scan_tokens();
         // tokens.iter().for_each(|x| println!("{:?}", x));
         if !self.error_reporter.had_error() {
-            let mut parser = Parser::new(tokens, self.error_reporter.as_mut());
+            let mut parser = Parser::new(tokens);
             println!("{}", parser.parse());
         }
     }
     pub fn run_file(&mut self, file: String) -> bool {
-        let contents = fs::read_to_string(file).unwrap();
-        self.run(contents);
+        let contents = fs::read_to_string(file.clone()).unwrap();
+        self.run(contents, file);
         self.error_reporter.had_error() // TODO: better error handling then boolean...
     }
 
@@ -44,7 +44,7 @@ impl Lox {
             match std.read_line(&mut buf).unwrap() {
                 0 => return,
                 _ => {
-                    self.run(buf.trim_end().to_string());
+                    self.run(buf.trim_end().to_string(), "stdin".to_string());
                     self.error_reporter.reset();
                 }
             }
