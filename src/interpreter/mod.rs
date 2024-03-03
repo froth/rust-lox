db41df472635079a6a9872f3622a88b5213ca044
Fred Roth <f-roth@megaera.de>
2024-03-03 22:03:55 +0100

    Another try at modeling local environments

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -5,15 +5,17 @@ pub mod printer;
 pub mod runtime_error;
 mod statement;
 
+use std::mem;
+
 use crate::ast::stmt::Stmt;
 
-use self::{environment::GlobalEnvironment, printer::Printer, runtime_error::RuntimeError};
+use self::{environment::Environment, printer::Printer, runtime_error::RuntimeError};
 
 type Result<T> = std::result::Result<T, RuntimeError>;
 #[derive(Default)]
 pub struct Interpreter {
     printer: Box<dyn Printer>,
-    environment: GlobalEnvironment,
+    environment: Environment,
 }
 
 impl Interpreter {
@@ -22,15 +24,28 @@ impl Interpreter {
             .into_iter()
             .try_for_each(|s| self.interpret_stmt(s))
     }
+
+    fn push_environment(&mut self) {
+        let new = Environment::default();
+        let old = mem::replace(&mut self.environment, new);
+        self.environment.parent = Some(Box::new(old))
+    }
+
+    fn pop_environment(&mut self) {
+        let parent = self.environment.parent.take();
+        let parent = parent.expect("Tried to pop global environment, bug in interpreter");
+        self.environment = *parent;
+    }
+
     #[cfg(test)]
     pub fn new(printer: Box<dyn Printer>) -> Self {
         Self {
             printer,
-            environment: GlobalEnvironment::default(),
+            environment: Environment::default(),
         }
     }
     #[cfg(test)]
-    pub fn with_env(printer: Box<dyn Printer>, environment: GlobalEnvironment) -> Self {
+    pub fn with_env(printer: Box<dyn Printer>, environment: Environment) -> Self {
         Self {
             printer,
             environment,
